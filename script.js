d589bd947edbd7affd848643622b8b5943cb02f4
Júnior <juniorsoaresssj@gmail.com>
2023-12-29 00:24:56 -0300

    adjust container witdh

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-function toggleMode(){
-  const html= document.documentElement
+function toggleMode() {
+  const html = document.documentElement
 
-  if(html.classList.contains("light"))
-html.classList.remove("light")
-  else(
-  html.classList.add("light")
-)
-const img=document.querySelector("#profile img")
+  if (html.classList.contains("light")) html.classList.remove("light")
+  else html.classList.add("light")
+  const img = document.querySelector("#profile img")
 
-if(img.classList.contains("light"))
-  img.setAttribute("src", "./assets/avatar-light.png")
-
-  else(
-    img.setAttribute("src", "./assets/avatar.png")
-  )
+  if (img.classList.contains("light"))
+    img.setAttribute("src", "./assets/avatar-light.png")
+  else img.setAttribute("src", "./assets/avatar.png")
 }
